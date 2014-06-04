4296bb798a394743cea838fe76caea324b52c04b
raunaqrox <raunaqrox@yahoo.co.in>
2014-06-04 06:58:07 +0530

    final

--- public/javascripts/reg.js
+++ public/javascripts/reg.js
@@ -70,8 +70,11 @@ $('#add').on('click',function(){
  $('#search').on('keyup', function(e){
    if(e.keyCode === 13) {
      var parameters = { search: $(this).val() };
-       $.get( '/search',parameters, function(data) {
-       	console.log(data);
+     var getAj=  $.get( '/search',parameters, function(data) {
+       		if(data.length===0){
+       			getAj.abort();
+       		}
+       		else
 		  $('#results').html("<a href="+data[0].url+'>'+data[0].title+'</a>');
   	});
     }
